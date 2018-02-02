3b19c7c363d66d1091d4f420ccc51bfe13313af1
Stefano Bettinelli <stefano.bettinelli85@gmail.com>
2018-02-02 02:34:23 +0100

    Feat: created Modal component
    Used for creation and editing of posts

---
+++ frontend/src/components/PostEditor.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle
+} from 'material-ui/Dialog';
+
+class FormDialog extends React.Component {  
+  render() {
+    const { open, handleClose } = this.props;
+
+    return (
+      <div>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To subscribe to this website, please enter your email address
+              here. We will send updates occationally.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleClose} color="primary">
+              Subscribe
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default FormDialog;
