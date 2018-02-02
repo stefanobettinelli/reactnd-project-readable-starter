f960be347b8f708ef40fc604f262452388528f60
Stefano Bettinelli <stefano.bettinelli85@gmail.com>
2018-02-02 17:22:19 +0100

    chore: added redux-thunk
    In order to update the store when using asyc interaction with server apis

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,21 +5,22 @@ import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import reducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-const composedEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composedEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   reducer,
-  composedEnhancers(
-    applyMiddleware(logger)
-  )
+  composedEnhancers(applyMiddleware(logger, thunk))
 );
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'));
+  document.getElementById('root')
+);
 registerServiceWorker();
