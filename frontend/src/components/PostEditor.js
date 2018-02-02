598f27f0c70a3972c4ec9babd0195fb833698b86
Stefano Bettinelli <stefano.bettinelli85@gmail.com>
2018-02-02 17:24:42 +0100

    Feat: PostEditor post field
    The PostEditor modal component has now all the required text fields for a post creation

--- frontend/src/components/PostEditor.js
+++ frontend/src/components/PostEditor.js
@@ -1,36 +1,88 @@
 import React from 'react';
+import { withStyles } from 'material-ui/styles';
 import Button from 'material-ui/Button';
 import TextField from 'material-ui/TextField';
+import Input, { InputLabel } from 'material-ui/Input';
+import { MenuItem } from 'material-ui/Menu';
+import Select from 'material-ui/Select';
+import { FormControl, FormHelperText } from 'material-ui/Form';
 import Dialog, {
   DialogActions,
   DialogContent,
-  DialogContentText,
   DialogTitle
 } from 'material-ui/Dialog';
+import { connect } from 'react-redux';
 
-class FormDialog extends React.Component {  
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120
+  },
+  selectEmpty: {
+    marginTop: theme.spacing.unit * 2
+  }
+});
+
+class FormDialog extends React.Component {
+  state = {
+    category: ''
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   render() {
-    const { open, handleClose } = this.props;
+    const { classes, open, handleClose } = this.props;
 
     return (
-      <div>
+      <form className={classes.container} autoComplete="off">
         <Dialog
           open={open}
           onClose={handleClose}
           aria-labelledby="form-dialog-title"
         >
-          <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+          <DialogTitle id="form-dialog-title">New Post</DialogTitle>
           <DialogContent>
-            <DialogContentText>
+            {/* <DialogContentText>
               To subscribe to this website, please enter your email address
               here. We will send updates occationally.
-            </DialogContentText>
+            </DialogContentText> */}
             <TextField
               autoFocus
               margin="dense"
-              id="name"
-              label="Email Address"
-              type="email"
+              id="author"
+              label="Author"
+              fullWidth
+            />
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="category">Category</InputLabel>
+              <Select
+                value={this.state.category}
+                onChange={this.handleChange}
+                input={<Input name="category" id="category-helper" />}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+            <TextField
+              margin="dense"
+              id="title"
+              label="Title"
+              fullWidth
+            />
+            <TextField
+              multiline
+              rows="10"
+              margin="dense"
+              id="body"
+              label="Body"
               fullWidth
             />
           </DialogContent>
@@ -39,13 +91,19 @@ class FormDialog extends React.Component {
               Cancel
             </Button>
             <Button onClick={handleClose} color="primary">
-              Subscribe
+              Submit
             </Button>
           </DialogActions>
         </Dialog>
-      </div>
+      </form>
     );
   }
 }
 
-export default FormDialog;
+function mapStateToProp() {
+
+}
+
+const PostEditor = withStyles(styles)(FormDialog);
+
+export default connect()(PostEditor);
