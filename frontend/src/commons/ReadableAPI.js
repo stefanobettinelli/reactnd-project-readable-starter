e80b2e46bc6bead9f066f4d2f3bcea7b7870e5eb
Stefano Bettinelli <stefano.bettinelli85@gmail.com>
2018-02-04 01:04:16 +0100

    chore: commons folder create
    It will contain common functionalities used in other parts of the code base

---
+++ frontend/src/commons/ReadableAPI.js
@@ -0,0 +1,22 @@
+const url = 'http://localhost:3001';
+const headers = { Authorization: 'readable-api-auth' };
+
+export const getAllCategories = () =>
+  fetch(`${url}/categories`, { headers })
+    .then(res => res.json())
+    .then(data => data.categories);
+
+export const getAllPosts = () =>
+  fetch(`${url}/posts`, { headers }).then(res => res.json());
+
+export const submitPost = post =>
+  fetch(`${url}/posts`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(post)
+  })
+    .then(res => res.json())
+    .then(data => console.log(data));
